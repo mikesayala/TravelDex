beec308680d2617461156c69437adc4fea9fb542
Mike Ayala <ayala.s.mike@gmail.com>
2021-09-14 12:50:17 -0700

    moved the add button on the results page from the left side to the right using d-flex justify-content-end and added some margin to make it flush with the accordion

--- client/pages/result.jsx
+++ client/pages/result.jsx
@@ -30,14 +30,14 @@ export default class Result extends React.Component {
                <img src={this.state.currentPlan.pictureUrl} className=" pictureUrl h-25 p-0 m-0" alt=""/>
              </div>
              <div className="col-12 h-100 col-sm-6">
-                <h2 className="m-3 plan me-3">
+                <h2 className="m-1 plan me-3">
                   {this.state.currentPlan.date}
                 </h2>
-                <h1 className="m-3 plan">
+                <h1 className="m-1 plan">
                   {this.state.currentPlan.planName}
                 </h1>
-                <a href={`#activityForm?planId=${this.state.currentPlanId}`}>
-                  <button className="mb-2">click me</button>
+                <a href={`#activityForm?planId=${this.state.currentPlanId}`} className="d-flex justify-content-end margin-3">
+                  <button className="mb-2 btn btn-primary">Add</button>
                 </a>
                 <Accordion plan={this.state.currentPlanId} />
              </div>
